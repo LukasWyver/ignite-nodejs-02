80182c8112637c5dfb036976b139777eacdb9291
Lukaswyver <lucas_sin5@hotmail.com>
2023-10-06 14:42:46 -0300

    Database - Treating  env variables with Zod

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,19 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transactions = await knex('transactions').select('*')
 
-  return tables
+  return transactions
 })
 
 app
   .listen({
     host: '0.0.0.0',
-    port: process.env.PORT ? Number(process.env.PORT) : 3333,
+    port: process.env.PORT ? Number(process.env.PORT) : env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running! 🚀')
